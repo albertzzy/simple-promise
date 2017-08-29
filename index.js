22b58d5f7654fcb555ec39b9f53a866f13b198ea
zzy <18299@etransfar.com>
2017-08-29 13:17:55 +0800

    update

--- index.js
+++ index.js
@@ -4,48 +4,84 @@ const symbolReject = Symbol('Promise#reject');
 
 class SimplePromise{
     constructor(fn){
+        if(typeof fn !== 'function'){
+            throw new Error('arg error');
+        }
+
         this.state = 'pending';
-
         this.promiseVal = null;
-
         this.handlers = [];
+
+        let resolve  = val => this[symbolResolve](val);
+        let reject = error => this[symbolReject](error);
+
+        fn(resolve,reject);
+
     }
 
-    [symbolResolve](result){
+    execute(type){
+        setTimeout(()=>{
+            this.handlers.forEach(handle => {
+                if(type === 'fullfilled'){
+                    handle.onFullfilled(this.promiseVal);
+
+                }else if(type === 'rejected'){
+                    handle.onRejected(this.promiseVal);
+
+                }
+            })
+        },0)
+    }
+
+    [symbolResolve](val){
         this.state = 'fullfilled';
+        this.promiseVal = val;
 
-        this.promiseVal = result;
-
+        this.execute(this.state);
     }
 
     [symbolReject](error){
         this.state = 'rejected';
-
         this.promiseVal = error;
+
+        this.execute(this.state);
     }
 
+    // then(promise) / then(onresolve,onreject)
+    then(onFullfilled,onRejected){
+        return new SimplePromise((resolve,reject) => {
 
-    thenCatch(){
+            if(onFullfilled instanceof SimplePromise){
+                let th = onFullfilled.then;
+                th.call(this,resolve,reject);
+                return;
+            }
 
+            if(this.state === 'pending'){
+                this.handlers.push({
+                    onFullfilled,
+                    onRejected
+                });
+                return;
+            }
 
-    }   
+            let fn = this.state === 'fullfilled'? onFullfilled : onRejected;
+            let oldValue = this.promiseVal;
 
-    then(resolve,reject){
+            try{
+                let newVal = fn(oldValue);
+                resolve(newVal);
+            }catch(e){
+                reject(oldValue);
+            }   
 
-
+        
+        })
     }
 
-    catch(reject){
-
-
+    catch(onRejected){
+        this.then(null,onRejected);
     }
 
 
 }
-
-
-new Promise((resolve,reject)=>{
-    foo();
-}).then(()=>{
-    
-})
